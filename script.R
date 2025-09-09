213653daee32e4d7011fa09dcd3fa31b28a4a663
placier moïse <moise.placier@gmail.com>
2025-09-09 10:26:41 +0200

    feature - facto fonctions

--- script.R
+++ script.R
@@ -1,4 +1,48 @@
-x <- 10
-y <- "Conflict! test 2  "
 
-z <- "erztzterz"
+
+facto <- 1
+for (i in 1:5) {
+  facto = i*facto
+}
+facto
+
+
+facto <- 1
+k <-1 
+while (k<=5) {
+  facto = k * facto;
+  k <- k+ 1
+} 
+facto 
+
+
+factFor <- function(n) {
+  facto <- 1
+  for (i in 1:n) {
+    facto = i * facto
+  }
+  return(facto)
+}
+
+factWhile <- function(n) {
+  k <- 1
+  facto <- 1
+  while (k <= n) {
+    facto = k * facto
+    
+    k <- k + 1
+  }
+  return(facto)
+}
+  
+
+facto <- factFor(5)
+facto
+  
+facto <- factWhile(1000)
+facto  
+  
+  
+  
+  
+  
